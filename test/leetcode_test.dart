5cc37167b2f98f9a5303011b470dac002a47a87a
Plague Fox <plugfox@gmail.com>
2022-08-08 22:27:57 +0400

    1480. Running Sum of 1d Array

--- test/leetcode_test.dart
+++ test/leetcode_test.dart
@@ -1,7 +1,9 @@
 import 'problems/add_two_integers.2235.test.dart' as add_two_integers_2235_test;
 import 'problems/richest_customer_wealth.1672.test.dart' as richest_customer_wealth_1672_test;
+import 'problems/running_sum_of_1d_array.1480.test.dart' as running_sum_of_1d_array_1480_test;
 
 void main() {
   add_two_integers_2235_test.main();
   richest_customer_wealth_1672_test.main();
+  running_sum_of_1d_array_1480_test.main();
 }
