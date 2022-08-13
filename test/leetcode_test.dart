6e7550ac0d2ae1042feacf52241c9cf7274116cb
Plague Fox <plugfox@gmail.com>
2022-08-13 23:29:16 +0400

    26. Remove Duplicates from Sorted Array

--- test/leetcode_test.dart
+++ test/leetcode_test.dart
@@ -9,6 +9,7 @@ import 'problem/middle_of_the_linked_list.876.test.dart' as middle_of_the_linked
 import 'problem/number_of_steps_to_reduce_a_number_to_zero.1342.test.dart'
     as number_of_steps_to_reduce_a_number_to_zero_1342_test;
 import 'problem/ransom_note.383.test.dart' as ransom_note_383_test;
+import 'problem/remove_duplicates_from_sorted_array.26.test.dart' as remove_duplicates_from_sorted_array_26_test;
 import 'problem/remove_element.27.test.dart' as remove_element_27_test;
 import 'problem/richest_customer_wealth.1672.test.dart' as richest_customer_wealth_1672_test;
 import 'problem/running_sum_of_1d_array.1480.test.dart' as running_sum_of_1d_array_1480_test;
@@ -30,5 +31,6 @@ void main() {
     duplicate_zeros_1089_test.main();
     squares_of_a_sorted_array_977_test.main();
     remove_element_27_test.main();
+    remove_duplicates_from_sorted_array_26_test.main();
   });
 }
---
+++ test/problem/remove_duplicates_from_sorted_array.26.test.dart
@@ -0,0 +1,20 @@
+import 'package:leetcode/src/problem/remove_duplicates_from_sorted_array.26.dart';
+import 'package:test/test.dart';
+
+void main() => group('remove_duplicates_from_sorted_array.26', () {
+      final f = RemoveDuplicatesFromSortedArray();
+
+      test('f([1,1,2])', () {
+        final list = <int>[1, 1, 2];
+        final result = f(list);
+        expect(list, containsAllInOrder(<int>[1, 2]));
+        expect(result, equals(2));
+      });
+
+      test('f([0,0,1,1,1,2,2,3,3,4])', () {
+        final list = <int>[0, 0, 1, 1, 1, 2, 2, 3, 3, 4];
+        final result = f(list);
+        expect(list, containsAllInOrder(<int>[0, 1, 2, 3, 4]));
+        expect(result, equals(5));
+      });
+    });
