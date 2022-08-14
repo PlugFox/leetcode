37579e0feefb3cf70062cbd3fc0fe8784acae942
Plague Fox <plugfox@gmail.com>
2022-08-15 00:24:46 +0400

    1346. Check If N and Its Double Exist - binary search and optimizations

--- lib/src/problem/check_if_n_and_its_double_exist.1346.dart
+++ lib/src/problem/check_if_n_and_its_double_exist.1346.dart
@@ -42,12 +42,88 @@ class CheckIfNAndItsDoubleExistLinear {
   bool call(List<int> arr) {
     if (arr.length < 2) return false;
     for (var i = 0; i < arr.length - 1; i++) {
-      final x = arr[i] / 2, y = arr[i] * 2;
-      for (var j = i + 1; j < arr.length; j++) {
-        if (arr[j] == x || arr[j] == y) return true;
-        continue;
+      final x = arr[i] * 2, y = arr[i] / 2;
+      if (x == 0) {
+        for (var j = i + 1; j < arr.length; j++) {
+          if (arr[j] == 0) return true;
+          continue;
+        }
+      } else if (y == y.truncate()) {
+        for (var j = i + 1; j < arr.length; j++) {
+          if (arr[j] == x || arr[j] == y) return true;
+          continue;
+        }
+      } else {
+        for (var j = i + 1; j < arr.length; j++) {
+          if (arr[j] == x) return true;
+          continue;
+        }
       }
     }
     return false;
+  }
+}
+
+class CheckIfNAndItsDoubleExistBinarySearch {
+  bool call(List<int> arr) {
+    if (arr.length < 2) return false;
+    final list = arr.toList(growable: false)..sort();
+    final length = list.length;
+
+    final firstPositive = () {
+      for (var i = 0; i < length; i++) {
+        if (list[i] > 0) return i;
+      }
+      return null;
+    }();
+    int? prev;
+
+    // Positive iteration
+    if (firstPositive != null) {
+      final iterateTo = length - 1;
+      for (var i = firstPositive; i < iterateTo; i++) {
+        if (list[i] == prev) continue;
+        if (_binarySearch(list, list[i] * 2, from: i + 1, to: length) != null) {
+          return true;
+        }
+        prev = list[i];
+      }
+    }
+
+    // Negative iteration
+    if (firstPositive != 0) {
+      for (var i = (firstPositive ?? length) - 1; i > 0; i--) {
+        if (list[i] == prev) continue;
+        if (_binarySearch(list, list[i] * 2, from: 0, to: i) != null) {
+          return true;
+        }
+        prev = list[i];
+      }
+    }
+
+    return false;
+  }
+
+  static int? _binarySearch<T extends Comparable<Object?>>(
+    List<T> list,
+    T value, {
+    int? from,
+    int? to,
+    int Function(T, T)? compare,
+  }) {
+    var min = from ?? 0, max = to ?? list.length;
+    compare ??= (a, b) => a.compareTo(b);
+    while (min < max) {
+      final mid = min + ((max - min) >> 1);
+      final comp = compare(list[mid], value);
+      if (comp == 0) {
+        return mid;
+      } else if (comp.isNegative) {
+        min = mid + 1;
+      } else {
+        max = mid;
+      }
+    }
+    return null;
   }
 }
--- test/problem/check_if_n_and_its_double_exist.1346.test.dart
+++ test/problem/check_if_n_and_its_double_exist.1346.test.dart
@@ -2,21 +2,51 @@ import 'package:leetcode/src/problem/check_if_n_and_its_double_exist.1346.dart';
 import 'package:test/test.dart';
 
 void main() => group('check_if_n_and_its_double_exist.1346', () {
-      final f = CheckIfNAndItsDoubleExistLinear();
+      final f1 = CheckIfNAndItsDoubleExistLinear();
+      final f2 = CheckIfNAndItsDoubleExistBinarySearch();
 
       test('f([])', () {
-        expect(f([]), isFalse);
+        {
+          expect(f1([]), isFalse);
+        }
+        {
+          expect(f2([]), isFalse);
+        }
       });
 
       test('f([10,2,5,3])', () {
-        expect(f([10, 2, 5, 3]), isTrue);
+        {
+          expect(f1([10, 2, 5, 3]), isTrue);
+        }
+        {
+          expect(f2([10, 2, 5, 3]), isTrue);
+        }
       });
 
       test('f([7,1,14,11])', () {
-        expect(f([7, 1, 14, 11]), isTrue);
+        {
+          expect(f1([7, 1, 14, 11]), isTrue);
+        }
+        {
+          expect(f2([7, 1, 14, 11]), isTrue);
+        }
       });
 
       test('f([3,1,7,11])', () {
-        expect(f([3, 1, 7, 11]), isFalse);
+        {
+          expect(f1([3, 1, 7, 11]), isFalse);
+        }
+        {
+          expect(f2([3, 1, 7, 11]), isFalse);
+        }
+      });
+
+      test('f([-10,12,-20,-8,15])', () {
+        {
+          expect(f1([-10, 12, -20, -8, 15]), isTrue);
+        }
+        {
+          expect(f2([-10, 12, -20, -8, 15]), isTrue);
+        }
       });
     });
