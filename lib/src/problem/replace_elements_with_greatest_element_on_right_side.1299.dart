0c4dab5f52bf090d999e06295604a88b5873077c
Plague Fox <plugfox@gmail.com>
2022-08-15 21:46:39 +0400

    1299. Replace Elements with Greatest Element on Right Side - update comment

--- lib/src/problem/replace_elements_with_greatest_element_on_right_side.1299.dart
+++ lib/src/problem/replace_elements_with_greatest_element_on_right_side.1299.dart
@@ -1,7 +1,8 @@
 /*
  * 1299. Replace Elements with Greatest Element on Right Side
  * 
- * Given an array arr, replace every element in that array with the greatest element among the elements to its right, and replace the last element with -1.
+ * Given an array arr, replace every element in that array with the greatest 
+ * element among the elements to its right, and replace the last element with -1.
  * 
  * After doing so, return the array.
  * 
