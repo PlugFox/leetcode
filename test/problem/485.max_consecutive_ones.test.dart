a648df22d18b4c98b7d5d3585bc1dfc6f81b35ed
Plague Fox <plugfox@gmail.com>
2022-08-18 14:45:59 +0400

    485. Max Consecutive Ones - add to corner cases

--- test/problem/485.max_consecutive_ones.test.dart
+++ test/problem/485.max_consecutive_ones.test.dart
@@ -4,6 +4,14 @@ import 'package:test/test.dart';
 void main() => group('max_consecutive_ones', () {
       final f = Solution().findMaxConsecutiveOnes;
 
+      test('f([0])', () {
+        expect(f([0]), equals(0));
+      });
+
+      test('f([1])', () {
+        expect(f([1]), equals(1));
+      });
+
       test('f([1,1,0,1,1,1])', () {
         expect(f([1, 1, 0, 1, 1, 1]), equals(3));
       });
