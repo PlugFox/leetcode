890a2650b3d8beeedc80b55b3a564a257fe6660d
Plague Fox <plugfox@gmail.com>
2022-08-20 11:31:19 +0400

    414. Third Maximum Number - replace with list

--- lib/src/problem/414.third_maximum_number.dart
+++ lib/src/problem/414.third_maximum_number.dart
@@ -47,40 +47,25 @@
 class Solution {
   int thirdMax(List<int> nums) {
     if (nums.isEmpty) throw ArgumentError.value(nums, 'nums', 'Array should contain at least one number');
-    var m1 = nums[0];
-    int? m2, m3;
-
-    bool setMax1(int n) {
-      if (m1 > n) return false;
-      if (m1 < n) {
-        m3 = m2;
-        m2 = m1;
-        m1 = n;
+    const maximusCount = 3;
+    final maximus = List<int?>.filled(maximusCount, null, growable: false);
+    for (var i = 0; i < nums.length; i++) {
+      var n = nums[i];
+      for (var j = 0; j < maximusCount; j++) {
+        final m = maximus[j];
+        if (m == null) {
+          maximus[j] = n;
+          break;
+        } else if (m < n) {
+          maximus[j] = n;
+          n = m;
+          continue;
+        } else if (m == n) {
+          break;
+        }
       }
-      return true;
     }
 
-    bool setMax2(int n) {
-      if (m2 != null && m2! > n) return false;
-      if (m2 == null || m2! < n) {
-        m3 = m2;
-        m2 = n;
-      }
-      return true;
-    }
-
-    bool setMax3(int n) {
-      if (m3 != null && m3! > n) return false;
-      if (m3 == null || m3! < n) {
-        m3 = n;
-      }
-      return true;
-    }
-
-    for (var i = 1; i < nums.length; i++) {
-      if (setMax1(nums[i]) || setMax2(nums[i]) || setMax3(nums[i])) continue;
-    }
-
-    return m3 == null || m2 == m3 ? m1 : m3!;
+    return maximus.last == null ? maximus.first! : maximus.last!;
   }
 }
