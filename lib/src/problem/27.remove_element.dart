b6d789a1cc72fe1471645a45ebbeac974fcb840c
Plague Fox <plugfox@gmail.com>
2022-08-22 18:46:59 +0400

    27. Remove Element - refactoring

--- lib/src/problem/27.remove_element.dart
+++ lib/src/problem/27.remove_element.dart
@@ -40,7 +40,8 @@
  * 
  * Input: nums = [3,2,2,3], val = 3
  * Output: 2, nums = [2,2,_,_]
- * Explanation: Your function should return k = 2, with the first two elements of nums being 2.
+ * Explanation: Your function should return k = 2,
+ * with the first two elements of nums being 2.
  * It does not matter what you leave beyond the returned k (hence they are underscores).
  * 
  * 
@@ -48,7 +49,8 @@
  * 
  * Input: nums = [0,1,2,2,3,0,4,2], val = 2
  * Output: 5, nums = [0,1,4,0,3,_,_,_]
- * Explanation: Your function should return k = 5, with the first five elements of nums containing 0, 0, 1, 3, and 4.
+ * Explanation: Your function should return k = 5,
+ * with the first five elements of nums containing 0, 0, 1, 3, and 4.
  * Note that the five elements can be returned in any order.
  * It does not matter what you leave beyond the returned k (hence they are underscores).
  *  
@@ -60,23 +62,8 @@
  * 0 <= val <= 100
  */
 
-class RemoveElementV1 {
-  int call(List<int> nums, int val) {
-    var count = 0;
-    final length = nums.length;
-    for (var i = length - 1; i >= 0; i--) {
-      if (nums[i] == val) {
-        nums.removeAt(i);
-      } else {
-        count++;
-      }
-    }
-    return count;
-  }
-}
-
-class RemoveElementV2 {
-  int call(List<int> nums, int val) {
+class Solution {
+  int removeElement(List<int?> nums, int val) {
     var i = 0, n = nums.length;
     while (i < n) {
       if (nums[i] != val) {
--- test/problem/27.remove_element.test.dart
+++ test/problem/27.remove_element.test.dart
@@ -2,55 +2,28 @@ import 'package:leetcode/src/problem/27.remove_element.dart';
 import 'package:test/test.dart';
 
 void main() => group('remove_element', () {
-      final f1 = RemoveElementV1();
-      final f2 = RemoveElementV2();
+      final f = Solution().removeElement;
 
       test('f([3,2,2,3], 3)', () {
-        {
-          final list = <int>[3, 2, 2, 3];
-          final result = f1(list, 3);
-          expect(list, isNot(contains(3)));
-          expect(list, containsAll(<int>[2, 2]));
-          expect(result, equals(2));
-        }
-        {
-          final list = <int>[3, 2, 2, 3];
-          final result = f2(list, 3);
-          expect(list, isNot(contains(3)));
-          expect(list, containsAll(<int>[2, 2]));
-          expect(result, equals(2));
-        }
+        final list = <int>[3, 2, 2, 3];
+        final result = f(list, 3);
+        expect(list, isNot(contains(3)));
+        expect(list, containsAll(<int>[2, 2]));
+        expect(result, equals(2));
       });
 
       test('f([0,1,2,2,3,0,4,2], 2)', () {
-        {
-          final list = <int>[0, 1, 2, 2, 3, 0, 4, 2];
-          final result = f1(list, 2);
-          expect(list, isNot(contains(2)));
-          expect(list, containsAll(<int>[0, 1, 4, 0, 3]));
-          expect(result, equals(5));
-        }
-        {
-          final list = <int>[0, 1, 2, 2, 3, 0, 4, 2];
-          final result = f2(list, 2);
-          expect(list, isNot(contains(2)));
-          expect(list, containsAll(<int>[0, 1, 4, 0, 3]));
-          expect(result, equals(5));
-        }
+        final list = <int>[0, 1, 2, 2, 3, 0, 4, 2];
+        final result = f(list, 2);
+        expect(list, isNot(contains(2)));
+        expect(list, containsAll(<int>[0, 1, 4, 0, 3]));
+        expect(result, equals(5));
       });
 
       test('f([3, 3], 3)', () {
-        {
-          final list = <int>[3, 3];
-          final result = f1(list, 3);
-          expect(list, isNot(contains(3)));
-          expect(result, equals(0));
-        }
-        {
-          final list = <int>[3, 3];
-          final result = f2(list, 3);
-          expect(list, isNot(contains(3)));
-          expect(result, equals(0));
-        }
+        final list = <int>[3, 3];
+        final result = f(list, 3);
+        expect(list, isNot(contains(3)));
+        expect(result, equals(0));
       });
     });
