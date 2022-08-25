5dac83d756266e6e815c2421af2aafb5da646e5a
Plague Fox <plugfox@gmail.com>
2022-08-25 12:18:23 +0400

    206. Reverse Linked List - add test

--- test/problem/206.reverse_linked_list.test.dart
+++ test/problem/206.reverse_linked_list.test.dart
@@ -9,6 +9,10 @@ void main() => group('remove_duplicates_from_sorted_array', () {
         expect(f(null), isNull);
       });
 
+      test('f([1])', () {
+        expect(f(ListNode<int>.of(<int>[1])), containsAllInOrder(<int>[1]));
+      });
+
       test('f([1,2])', () {
         expect(f(ListNode<int>.of(<int>[1, 2])), containsAllInOrder(<int>[2, 1]));
       });
