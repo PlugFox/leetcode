7d5e250b1fb38d6aa9a738d9802d0539394b4531
Plague Fox <plugfox@gmail.com>
2022-08-28 23:53:57 +0400

    328. Odd Even Linked List - Refactoring

--- test/problem/328.odd_even_linked_list.test.dart
+++ test/problem/328.odd_even_linked_list.test.dart
@@ -2,11 +2,15 @@ import 'package:leetcode/src/problem/328.odd_even_linked_list.dart';
 import 'package:leetcode/src/structure/list_node.dart';
 import 'package:test/test.dart';
 
-void main() => group('longest_increasing_subsequence', () {
+void main() => group('odd_even_linked_list', () {
       final f = Solution().oddEvenList;
 
       test('f([])', () {
         expect(f(null)?.toList(), isNull);
+      });
+
+      test('f([1,2,3,4,5,6,7,8])', () {
+        expect(f(ListNode<int>.of([1, 2, 3, 4, 5, 6, 7, 8]))?.toList(), equals(<int>[1, 3, 5, 7, 2, 4, 6, 8]));
       });
 
       test('f([1,2,3,4,5])', () {
